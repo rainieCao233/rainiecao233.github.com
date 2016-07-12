baec6e7b12192014960bbfdbc471a590f51b2b16
ycao <caoyue@eastmoney.com>
2016-07-12 17:40:32 +0800

    添加tab切换功能

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+;(function($){
+	$(".tab_btn").click(function(e){
+		$(".tab_btn").each(function(index,ele){
+			$(ele).removeClass("tab_checked");
+		});
+		$(e.target).addClass("tab_checked");
+	});
+})(jQuery) 
