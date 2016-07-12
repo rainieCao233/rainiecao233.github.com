33c437505b3c76f9c17c446483ada847e408a485
ycao <caoyue@eastmoney.com>
2016-07-12 17:40:32 +0800

    修改样式

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
-console.log('This would be the main JS file.');
+;(function($){ 
+
+    $("a.nav_tab").click(function(e) {
+		$("a.nav_tab").each(function(index, ele){
+			$(ele).removeClass("on");
+		});
+		$(e.target).parent().addClass("on");
+
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({
+                    scrollTop: targetOffset
+                },
+                500);
+                return false;
+            }
+        }
+    });
+
+    // todo：当scroll发生改变时，监听tab的on状态自动切换
+
+})(jQuery) 
