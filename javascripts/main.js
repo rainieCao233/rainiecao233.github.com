d0266dcc11976e78d166b257679f081f4927b14e
ycao <caoyue@eastmoney.com>
2016-07-14 11:27:27 +0800

    添加主题切换功能

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,6 +21,53 @@
         }
     });
 
-    // todo£ºµ±scroll·¢Éú¸Ä±äÊ±£¬¼àÌıtabµÄon×´Ì¬×Ô¶¯ÇĞ»»
+    // todo: a.nav_tabçš„onçŠ¶æ€æ ¹æ®scrollå˜åŒ–
+
+    // åˆ‡æ¢ä¸»é¢˜
+    $("a.theme_color").click(function(e){
+        var _id = e.target.id;
+
+        $("a.theme_color").removeClass("active");
+        $(e.target).addClass("active");
+        
+        switch(e.target.id)
+        {
+            case 'purple':
+                $(".welcome_part").css({"background":"url(./images/pic01.jpg) no-repeat","background-size":"100% 100%"});
+                $(".sidebar").css("background","rgb(109, 85, 115)");
+                $(".mask").css("background","rgba(109, 85, 115, 0.3)");
+                break;
+            // case 'green':
+            //     $(".welcome_part").css({"background":"url(./images/pic02.jpg) no-repeat","background-size":"100% 100%"});
+            //     $(".sidebar").css("background","rgb(89, 117, 89)");
+            //     $(".mask").css("background","rgba(172, 210, 172, 0.3)");
+            //     break;
+            case 'red':
+                $(".welcome_part").css({"background":"url(./images/pic03.jpg) no-repeat","background-size":"100% 100%"});
+                $(".sidebar").css("background","rgb(134, 98, 98)");
+                $(".mask").css("background","rgba(216, 180, 180, 0.3)");
+                break;
+            case 'blue':
+                $(".welcome_part").css({"background":"url(./images/pic04.jpg) no-repeat","background-size":"100% 100%"});
+                $(".sidebar").css("background","rgb(85, 115, 115)");
+                $(".mask").css("background","rgba(108,148,148,0.3)");
+                break;
+            case 'gray':
+                $(".welcome_part").css({"background":"url(./images/pic05.jpg) no-repeat","background-size":"100% 100%"});
+                $(".sidebar").css("background","rgb(37, 37, 37)");
+                $(".mask").css("background","rgba(0, 0, 0, 0.3)");
+                break;
+        }
+    });
+
+    // éšè—styleé€‰æ‹©æ¡†
+    $("a.switcher").click(function(){
+        var ml = $(".custumize_style").css("margin-left");
+        if(ml == '0px'){
+            $(".custumize_style").animate({marginLeft:"-200px"});
+        }else{
+            $(".custumize_style").animate({marginLeft:"0px"}); 
+        }
+    });
 
 })(jQuery) 
